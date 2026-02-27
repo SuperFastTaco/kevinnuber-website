f1bc5412bbf8db10efdbbd56aed8efaa82a7dc6f
SuperFastTaco <kevin@kevinnuber.com>
2026-02-26 19:27:17 -0800

    Add files via upload

---
+++ index.css
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Process from './pages/Process';
+import Blog from './pages/Blog';
+import Personal from './pages/Personal';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="process" element={<Process />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="personal" element={<Personal />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
